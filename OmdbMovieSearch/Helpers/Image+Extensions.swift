bbc4395af404f1445bdac83aff8c6e06e4ae3385
Prem Shankar <prem.rg2005@gmail.com>
2024-12-07 14:40:18 +0800

    SearchBArView and Image extensions added to proj

---
+++ OmdbMovieSearch/Helpers/Image+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Image+Extensions.swift
+//  OmdbMovieSearch
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 7/12/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    static let sysImage = SystemImageNames()
+}
+
+struct SystemImageNames {
+    let searchImage = Image(systemName: "magnifyingglass")
+    let clearSearch = Image(systemName: "xmark.circle.fill")
+}

cc6dd60895e95ffef62b81dba7fb3ec5d70f712d
Prem Shankar <prem.rg2005@gmail.com>
2024-12-07 08:34:01 +0800

    Initial Commit

---
+++ OmdbMovieSearch/OmdbMovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmdbMovieSearchApp.swift
+//  OmdbMovieSearch
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OmdbMovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OmdbMovieSearchUITests/OmdbMovieSearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OmdbMovieSearchUITestsLaunchTests.swift
+//  OmdbMovieSearchUITests
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 7/12/24.
+//
+
+import XCTest
+
+final class OmdbMovieSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

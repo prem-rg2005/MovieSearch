e917ac38bfda715fca62136546135df16bca3af2
Prem Shankar <prem.rg2005@gmail.com>
2024-12-07 15:34:10 +0800

    Added MovieListView & its viewModel

---
+++ OmdbMovieSearch/View Models/MovieListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListViewModel.swift
+//  OmdbMovieSearch
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 7/12/24.
+//
+
+import Foundation
+import Combine
+
+class MovieListViewModel: ObservableObject {
+    
+}

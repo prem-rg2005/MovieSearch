cee8e6fb46153996b9916ea594ffeaa52e4d1cd1
Prem Shankar <prem.rg2005@gmail.com>
2024-12-07 14:54:02 +0800

    Added MovieListCell to display individual data

---
+++ OmdbMovieSearch/Views/MovieListCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListCell.swift
+//  OmdbMovieSearch
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 7/12/24.
+//
+
+import SwiftUI
+
+struct MovieListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListCell()
+}

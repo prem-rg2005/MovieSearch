dfd22a7b37c2c6d49bb4e3be896bc2942ce4883f
Prem Shankar <prem.rg2005@gmail.com>
2024-12-07 15:25:22 +0800

    Sample UI for MovieListCell

--- OmdbMovieSearch/Views/MovieListCell.swift
+++ OmdbMovieSearch/Views/MovieListCell.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct MovieListCell: View {
+    
+    @State var movie: Movie = .init(title: "Harry Potter and the Deathly Hallows: Part 2", year: "2011", id: "tt1201607", type: "movie", poster: "https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImage(url: URL(string: movie.poster)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .aspectRatio(contentMode: .fit)
+                } else if phase.error != nil {
+                    Text("No image available")
+                } else {
+                    Image(systemName: "photo")
+                }
+            }
+            .frame(width: 100)
+            .cornerRadius(15)
+            
+            VStackLayout(alignment: .leading) {
+                Text(movie.title)
+                    .font(.headline)
+                    .foregroundColor(Color.blue)
+                Text("Year: \(movie.year)")
+                    .font(.subheadline)
+                    .foregroundColor(Color.themeColor.secondaryText)
+            }
+            .padding(.leading, 8)
+        }
+        .padding()
+        .background(Color.themeColor.secondaryText)
+        .cornerRadius(5)
+        .padding(.horizontal)
     }
 }
 
